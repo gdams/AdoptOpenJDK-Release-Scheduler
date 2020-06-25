71ef0b1d82f53f9803855e607d58a75cc34fa1ad
Travis CI User <travis@example.org>
2020-06-25 12:05:18 +0000

    Updates

---
+++ precache-manifest.ea20ae7c67c7161b37fbfc88ea6bfe2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa3604a3b82fb5c0b61bdd67372c2bc",
+    "url": "/AdoptOpenJDK-Release-Scheduler/index.html"
+  },
+  {
+    "revision": "045ceccec4b7d0c2e6fc",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/css/2.1b1c968b.chunk.css"
+  },
+  {
+    "revision": "8310535ae4dd8a8bd6d8",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/css/main.ae21c90c.chunk.css"
+  },
+  {
+    "revision": "045ceccec4b7d0c2e6fc",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/js/2.a4b0763b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/js/2.a4b0763b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8310535ae4dd8a8bd6d8",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/js/main.fffefc34.chunk.js"
+  },
+  {
+    "revision": "8b9f3a63a5078cbb411e",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/js/runtime-main.4dce20ed.js"
+  },
+  {
+    "revision": "07e2ff5080eebae65c7f9f119aa5722a",
+    "url": "/AdoptOpenJDK-Release-Scheduler/static/media/adopt_logo_white.07e2ff50.png"
+  }
+]);
